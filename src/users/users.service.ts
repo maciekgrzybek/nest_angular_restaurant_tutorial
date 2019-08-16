87628dc0f6fdd423a24c45a19394e979928d5f01
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-08-16 17:04:15 +0100

    Add user interface

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users: User[];
+
+  constructor() {
+    this.users = [
+      {
+        id: 1,
+        username: 'Maciek',
+        password: 'somePass',
+      },
+      {
+        id: 2,
+        username: 'Jureas',
+        password: 'mmapanowie',
+      },
+      {
+        id: 1,
+        username: 'Waldek',
+        password: 'yolo',
+      },
+    ];
+  }
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username);
+  }
+}
