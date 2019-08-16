f4dd91e5ec798af35ef7b9ddaa366593dcb02509
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-08-16 16:32:12 +0100

    Generate auth and users modules / install passports packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { ItemsController } from './items/items.controller';
 import { ItemsService } from './items/items.service';
 import { ShoppingCartController } from './shopping-cart/shopping-cart.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController, ItemsController, ShoppingCartController],
-  providers: [AppService, ItemsService],
+  providers: [AppService, ItemsService, UsersService],
 })
 export class AppModule {}
